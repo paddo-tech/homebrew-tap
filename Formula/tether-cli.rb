157619125398ce22a9107dcb5919ddd21d135101
Paddo <653385+paddo@users.noreply.github.com>
2025-12-05 00:28:00 +1000

    tether-cli 1.0.3
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/tether-cli.rb
+++ Formula/tether-cli.rb
@@ -1,8 +1,8 @@
 class TetherCli < Formula
   desc "Sync dotfiles and packages across machines"
   homepage "https://github.com/paddo-tech/tether-cli"
-  url "https://github.com/paddo-tech/tether-cli/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "57a07ab593fc1103c8c751a22f53f674f875c27244575b967c330749c5f218e5"
+  url "https://github.com/paddo-tech/tether-cli/archive/refs/tags/v1.0.3.tar.gz"
+  sha256 "65c70e9eb9dc0ca7901564911c5f884364f724daeeda2d3189d2241ba3eec573"
   license "MIT"
 
   depends_on "rust" => :build
